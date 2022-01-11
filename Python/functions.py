23e7aca0fee502272bc6e34636a0c2dd6a24a62f
Abir835 <abirhasan.raj.bd@gmail.com>
2022-01-12 00:57:48 +0600

    <learn> function types of function like positional argument and pass by value and arbitrary argument

---
+++ Python/functions.py
@@ -0,0 +1,28 @@
+# arbitrary number of argument
+# def Student(*student):
+#     for i in student:
+#         print(i)
+#
+#
+# Student("abir", "hasan")
+
+# pass by reference
+
+# a = [1, 2, 3, 4]
+#
+#
+# def Number(a):
+#     a[0] = 5
+#     a[3] = 100
+#
+#
+# Number(a)
+# print(a)
+
+# positional argument
+
+# def student(name, age):
+#     print(name, age)
+#
+#
+# student(age=23, name="hello")
