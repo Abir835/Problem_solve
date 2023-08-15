b8c498bca790d542a61ab9c3fd6db7e1623d9e97
abir835 <abirhasan.raj.bd@gmail.com>
2023-08-16 00:48:21 +0600

    insert value count length remove at insert as index

--- Algorithm/LinkedList/linked_list.py
+++ Algorithm/LinkedList/linked_list.py
@@ -35,6 +35,56 @@ class LinkedList:
 
         itr.next = Node(new_data)
 
+    def insert_value(self, data):
+        self.head = None
+        for data_ in data:
+            self.push_end(data_)
+
+    def count_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    def remove_at(self, index):
+        if index < 0 or index >= self.count_length():
+            raise Exception('invalid index')
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index-1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    def insert_at(self, data, index):
+        if index < 0 or index >= self.count_length():
+            raise Exception('invalid index')
+
+        if index == 0:
+            self.push_begining(data)
+            return
+
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data)
+                node.next = itr.next
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
+
     def printList(self):
         temp = self.head
         while (temp):
@@ -43,13 +93,18 @@ class LinkedList:
 
 
 llist = LinkedList()
-llist.push_end(20)
-llist.push_end(4)
-llist.push_end(15)
-llist.push_end(85)
-
-print("Given linked list")
-llist.printList()
-llist.reverse()
-print("\nReversed linked list")
+# llist.push_end(20)
+# llist.push_end(4)
+# llist.push_end(15)
+# llist.push_end(85)
+#
+# print("Given linked list")
+# llist.printList()
+# llist.reverse()
+# print("\nReversed linked list")
+llist.insert_value(['abir', 'haddu', 'guddu'])
+print(llist.count_length())
+# llist.remove_at(1)
+print(llist.count_length())
+llist.insert_at('hasan', 1)
 llist.printList()
